dde7f52a72c93a26d6f6d5c15e735bab6bd2b8a5
sumit rathore <sumitrathore1313@gmail.com>
2017-10-23 17:55:47 +0530

    characterquantization

--- SkrResumeParser/Test/execute.py
+++ SkrResumeParser/Test/execute.py
@@ -1,6 +1,1 @@
-from VectorProcessing import GloveSentenceVector
-
-Vector_class = GloveSentenceVector('/home/sumit/PycharmProjects/ResumeParser/Data/resume_segments/Level0/',
-                                       save_model_path='/home/sumit/PycharmProjects/ResumeParser/Data/resume_segments/Test/')
-sen2vec = Vector_class.get_avg_sentenceVector()
-print len(sen2vec)
+import tensorflow
